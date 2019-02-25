2d4721f0b82ef6a0ae0932eee19047e61f42902f
ndneubauer <ndneubauer@gmail.com>
2019-02-25 21:51:00 +1000

    Updated API

--- bookblog_api/Program.cs
+++ bookblog_api/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace bookblog_api
 {
@@ -19,6 +12,7 @@ namespace bookblog_api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>();
     }
 }
--- bookblog_api/Startup.cs
+++ bookblog_api/Startup.cs
@@ -26,6 +26,7 @@ namespace bookblog_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,8 @@ namespace bookblog_api
             {
                 app.UseHsts();
             }
+
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("x-custom-header"));
 
             app.UseHttpsRedirection();
             app.UseMvc();
